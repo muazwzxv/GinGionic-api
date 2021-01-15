bf535c7e88e47b5681039c130b25aee98c083e19
Muaz Wazir <muazwazir@gmail.com>
2021-01-15 18:18:47 +0800

    db handler

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-	r := gin.Default()
+	router := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
+	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
 	})
 
-	r.Run()
+	router.Run()
 }
---
+++ models/setup.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	db, err := gorm.Open("mysql", "muaz:Muaz@mysql123/gormLearn?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	db.AutoMigrate(&Book{})
+	defer db.Close()
+
+	DB = db
+}
