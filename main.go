d272a3013b81e7424ecf9516843316a16bb397f9
Muaz Wazir <muazwazir@gmail.com>
2021-01-17 01:11:40 +0800

    clean main

--- main.go
+++ main.go
@@ -3,29 +3,9 @@ package main
 import (
 	"Go-Learn-API/controllers"
 	"Go-Learn-API/models"
-	"log"
-
-	"github.com/spf13/viper"
 
 	"github.com/gin-gonic/gin"
 )
-
-func viperEnv(key string) string {
-	viper.SetConfigFile(".env")
-
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Fatalf("error while reading config file %s", err)
-	}
-
-	value, ok := viper.Get(key).(string)
-
-	if !ok {
-		log.Fatalf("invalid type insertion")
-	}
-
-	return value
-}
 
 func main() {
 	router := gin.Default()
