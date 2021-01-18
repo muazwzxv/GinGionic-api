56239266acee225ac1a9e3bd3e9e67fea219536b
Muaz Wazir <muazwazir@gmail.com>
2021-01-18 23:44:33 +0800

    validate email

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module Go-Learn-API
 go 1.15
 
 require (
+	github.com/badoux/checkmail v1.2.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/gorm v1.9.16
--- models/user.go
+++ models/user.go
@@ -1,7 +1,25 @@
 package models
 
+import (
+	"errors"
+
+	"github.com/badoux/checkmail"
+)
+
 // User := user properties
 type User struct {
 	ID    uint64 `gorm:"primary_key;auto_increment" json:"id"`
 	Email string `gorm:"size:255;not null;unique" json:"email"`
+}
+
+// ValidateEmail := validate incoming email
+func (s *Server) ValidateEmail(email string) error {
+	if email == "" {
+		return errors.New("email is required")
+	} else {
+		if err := checkmail.ValidateFormat(email); err != nil {
+			return errors.New("Invalid email")
+		}
+	}
+	return nil
 }
